e11cc462a93b195e3162cc4f92b0a4f6765c6152
phoaxy <54962416+phoaxy@users.noreply.github.com>
2021-02-27 19:22:51 -0500

    Added status messages when toggling on and off

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 void menu()
 {
-		system("Color a");
+		system("Color c");
 cout << "	  ____  _____  __  _         __  _      ____   __  __  _    ___  ____  \n";
 cout << "	 /    ||     ||  |/ ]       /  ]| |    |    | /  ]|  |/ ]  /  _]|    \  \n";
 cout << "	|  o  ||   __||  ' /       /  / | |     |  | /  / |  ' /  /  [_ |  D  ) \n";
@@ -13,7 +13,7 @@ cout << "	|  _  ||   _] |     |    /   \_ |     | |  /   \_ |     \|   [_ |    \
 cout << "	|  |  ||  |   |  .  |    \     ||     | |  \     ||  .  ||     ||  .  \ \n";
 cout << "	|__|__||__|   |__|\_|     \____||_____||____\____||__|\_||_____||__|\_| \n\n";
                                                                        
-	cout << "AFKClicker (COMBAT UPDATE VERSION) by Jordan/CLOUT3D\n\n";
+	cout << "AFKClicker (COMBAT UPDATE VERSION) by Jordan/CLOUT3D\n\n\n";
 	cout << "DO NOT ENABLE LEFT CLICK AUTO CLICKER ON SERVERS THIS WILL RESULT IN A BAN.\n\n";
 	cout << "Left Click -> Press 'X' to enable and 'Z' to disable Left Click Auto Click.\n\n";
 	cout << "Right Click -> Press 'V' to enable and 'C' to disable Right Click Auto Click.\n\n";
@@ -29,25 +29,37 @@ void clicker()
 		//Left Click
 		if (GetAsyncKeyState('X')) //toggle on (lmb)
 		{
+			system("Color 0a");
+			cout << "LMB Clicker: ON (Clicking 1cps)\n\n";
 			click = true; 
+			Sleep(500);
 		}
 		else if (GetAsyncKeyState('Z')) //toggle off (lmb)
 		{
-			click = false;
+			system("Color 0c");
+			cout << "LMB Clicker: OFF\n\n";
+			click = false; 
+			Sleep(500);
 		}
 		if (click == true) 
 		{
 			mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
 			mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
-			Sleep(1000); //Adujst Speed | (Warning settings this value below 1 WILL cause come crashes. Try at your own risk. THE LOWER THE VALUE THE FASTER)
+			Sleep(1000);
 		}
 		//Right Click
 				if (GetAsyncKeyState('V')) //toggle on (rmb)
 		{
+			system("Color 0a");
+			cout << "RMB Clicker: ON (Clicking 63+ cps)\n\n";
+			Sleep(500);
 			Rclick = true; 
 		}
 		else if (GetAsyncKeyState('C')) //toggle off (rmb)
 		{
+			system("Color 0c");
+			cout << "RMB Clicker: OFF\n\n";
+			Sleep(500);
 			Rclick = false;
 		}
 		if (Rclick == true)
